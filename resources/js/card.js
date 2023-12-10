4bc63a848e52c3b3df7176fde4d7367f060c4a13
N0NEWHY <1270112393@qq.com>
2023-12-11 07:06:58 +0800

    1

---
+++ resources/js/card.js
@@ -0,0 +1,23 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const cards = document.querySelectorAll(".card");
+
+//     cards.forEach((card) => {
+//         card.addEventListener("click", function () {
+//             // Toggle the 'active' class for the clicked card
+//             card.classList.toggle("active");
+
+//             // Toggle the display of the brief and detail sections
+//             const brief = card.querySelector(".brief");
+//             const detail = card.querySelector(".detail");
+
+//             if (card.classList.contains("active")) {
+//                 brief.style.display = "none";
+//                 detail.style.display = "block";
+//             } else {
+//                 brief.style.display = "block";
+//                 detail.style.display = "none";
+//             }
+//         });
+//     });
+// });
+
